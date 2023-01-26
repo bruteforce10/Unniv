dd7c4207e4992568be43d58e76919799e0b4fc66
Firdi <Firdi>
2023-01-26 18:01:12 +0700

    feature-program

--- script.js
+++ script.js
@@ -13,7 +13,29 @@ button.addEventListener("mouseover", function () {
   let y = Math.floor(Math.random() * 100) + buttonMau.offsetTop;
 
   this.style.left = x + "px";
-  this.style.top = x + "px";
+  this.style.top = y + "px";
+
 });
 
 // https://www.youtube.com/watch?v=GJrR58QGenw
+
+const inputJujur = document.querySelector('#inputJujur');
+
+inputJujur.addEventListener('change', function() {
+  this.value = "TIDAK DONG";
+})
+
+
+const berubah = document.querySelector('#berubah span');
+const range = document.querySelector('#range');
+
+range.addEventListener('change', function(){
+  berubah.innerHTML = "<b>SAYANG</b>";
+})
+
+
+
+
+
+
+
